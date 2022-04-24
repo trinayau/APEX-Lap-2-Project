ad4c54b740665eb42f1915cc9d57e33ff41da4d2
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-04-24 20:17:06 +0100

    boiler plate models

---
+++ api/controllers/controller.js
@@ -0,0 +1,43 @@
+const Model = require('../models/Model');
+
+async function index (req, res) {
+    try {
+        const object = await Model.all;
+        res.status(200).json(object)
+    } catch (err) {
+        res.status(500).json({err})
+    }
+}
+
+async function show (req, res) {
+    try {
+        const object = await Model.getById(req.params.id);
+        res.status(200).json(object)
+    } catch (err) {
+        res.status(404).json({err})
+        console.log(err)
+    }
+}
+
+async function create (req, res) {
+    try {
+        const object = await Model.create(req.body);
+        res.status(201).json(object)
+    } catch (err) {
+        console.log(err)
+        res.status(422).json({err})
+    }
+}
+
+async function destroy (req, res) {
+    try {
+        const object = await Model.getById(req.params.id);
+        const resp = await object.destroy();
+        res.status(204).end();
+    } catch (err) {
+        res.status(404).json({err});
+        console.log(err)
+    };
+}
+
+module.exports = { index, show, create, destroy }
--- api/models/Model.js
+++ api/models/Model.js
@@ -1,1 +1,57 @@
-//test comment
+//require database
+
+//require other classes if needed
+
+moudle.exports = class ClassExample {
+    constructor(data){
+        this.id = data.id;
+        this.name = data.name;
+    }
+
+    static get all(){
+        return new Promise(async (resolve, reject) => {
+            try {
+                console.log("hello");
+                //let objectData = await // query database
+                //let object = objectData.map(o => new Object(o));
+            } catch (err) {
+                reject(err);
+            }
+        })
+    }
+
+    static getById(id){
+        return new Promise (async (resolve, reject) => {
+            try {
+                //let objectData = await // query database by [id]
+                //let object = objectData.map(o => new Object(o));
+                //resolve (object);
+            } catch (err) {
+                reject('object not found id');
+            }
+        });
+    }
+
+    static async create(objectData){
+        return new Promise (async (resolve, reject) => {
+            try {
+                //const {id, title, etc} = objectData
+                //let result = await // insert into database [id, title, etc]
+                //resolve (result);
+            } catch (err) {
+                reject('object could not be created');
+            }
+        });
+    };
+
+    destroy(){
+        return new Promise(async(resolve, reject) => {
+            try {                
+                //let result = await // delete from database [this.id]
+                //resolve (objected was deleted);
+            } catch (err) {
+                reject('object could not be deleted')
+            }
+        })
+    };
+}
