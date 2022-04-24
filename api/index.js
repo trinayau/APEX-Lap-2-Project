084a5e27bd525a2ff0d39acc5997baa8c70d31da
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-04-24 20:06:25 +0100

    Docker YAML file updated, connection to mongodb established and basic user schema set up

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const server = require('./server')
+const mongoose = require('mongoose');
+
+const mongodbURI = process.env.DB_CONNECTION;
+const port = process.env.PORT || 3000;
+
+mongoose.connect(mongodbURI)
+    .then(res => {
+        console.log('Connected to DB');
+        server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
+    })
+    .catch(err => console.log(err));
---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
