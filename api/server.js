918a82c824fae8a37fae6d0319866fe214ea7a65
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-25 11:35:07 +0100

    commented out bcrypt

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 
 const app = express();
