4f5bd1c31943f6f5e766dd302854b49bffe1d0b5
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-26 15:27:02 +0100

    nothing

--- api/server.js
+++ api/server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res) => res.render('index'))
-app.get('/hero', (req, res) => res.render('herocarousel'))
 
 app.use('/habits', habitRoutes)
 
