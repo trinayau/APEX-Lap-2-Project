37e45e2214e296ed8377e7221b8dac7c9021c312
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-04-27 17:42:32 +0100

    game controllers

--- api/routes/gameRoutes.js
+++ api/routes/gameRoutes.js
@@ -2,26 +2,13 @@ const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/gameController')
 
-
-// router.get('/', objectController.index)
-// router.get('/:id', objectController.show)
-// router.post('/', objectController.create)
-// router.delete('/:id', objectController.destroy)
-
 router.route('/')
-    .get(gameController.index)
-    .post(gameController.create)
+    .get(gameController.getAll)         //done
+    .post(gameController.createGame)    //done
 
 router.route('/:id')
-    .get(gameController.show)
-    .post(gameController.create)
-    .put(gameController.update)
+    .get(gameController.showGameById)   //done           
+    .put(gameController.updateGame)     //in progress
 
-// router.route('/users')
-//     .get(objectController.index)
-//     .post(objectController.addUser)
-
-// router.route('/users/login')
-//     .post(objectController.getUser)
 
 module.exports = router;
