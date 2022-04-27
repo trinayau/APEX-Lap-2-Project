9b968cef119d600e5d76e3b5d6393fdcf5959ac0
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-04-27 13:19:20 +0100

    Updated authcontrollers and other auth stuff

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-const server = require('./server')
+const server = require('./server');
+const axios = require('axios');
 const mongoose = require('mongoose');
 
 const mongodbURI = process.env.DB_CONNECTION;
@@ -7,6 +8,13 @@ const port = process.env.PORT || 3000;
 mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(res => {
         console.log('Connected to DB');
+        const params = new URLSearchParams({
+            client_id: "dxk0rvozg2yjrvaylnawg1kh3egd6h",
+            client_secret: "j58stlrvw317jjispg3wdgdotwnc6f",
+            grant_type: "client_credentials"
+        })
+        axios.post(`https://id.twitch.tv/oauth2/token?${params.toString()}`)
+            .then(resp => module.exports = resp.data['access_token'])
         server.listen(port, () => console.log(`\nServer listening on port http://localhost:${port}\n`));
     })
     .catch(err => console.log(err));
--- api/models/User.js
+++ api/models/User.js
@@ -1,96 +1,121 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const { isEmail } = require('validator');
-const uniqueValidator = require('mongoose-unique-validator');
-<<<<<<< HEAD
+const Schema = require('./Schema');
 
-const habitSchema = new mongoose.Schema({
-  habitId: {
-    type: Number,
-    required: true
-  },
-  habitName: {
-    type: String,
-    required: true,
-    maxlength: [50, 'Game title can only be up to 50 characters']
-  },
-  habitType: {
-    type:String,
-    required: true
-  },
-  habitComplete: {
-    type: Boolean,
-    required: true
-  }
-})
-
-const gameSchema = new mongoose.Schema({
-  gameID: {
-    type: Number,
-    required: false,
-  },
-  gameName: {
-    type: String,
-    required: true,
-    lowercase: true,
-    maxlength: [50, 'Game title can only be up to 50 characters']
-  },
-  gameImg: {
-    type: String,
-    required: false,
-  },
-  habits: [habitSchema]
-})
-=======
->>>>>>> bce4a278793cbdb97a5e83115b7a77d6658cd687
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    unique: true,
-    lowercase: true,
-    maxLength: [20, 'Username can only be up to 20 characters'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
-  },
-  password: {
-    type: String,
-    required: [true, 'password required'],
-    minlength: [5, 'Password must be at least 5 characters'],
-<<<<<<< HEAD
-  },
-  games: [gameSchema]
-=======
-  }
->>>>>>> bce4a278793cbdb97a5e83115b7a77d6658cd687
-});
-
-userSchema.pre("save", async function(next){
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);  
-    next();
-})
-
-userSchema.statics.login = async function(email, password) {
-  const user = await this.findOne({ email: email});
-  if (user){
-    const auth = await bcrypt.compare(password, user.password)
-    if (auth){
-      return user;
+module.exports = class User {
+    constructor(data) {
+        this.email = data.email;
+        this.username = data.username;
+        this.password = data.password;
     }
-    throw Error('Invalid password')
-  }
-  throw Error('Incorrect email')
+
+    static async findByEmail(email) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const user = Schema.User.findOne({ email: email });
+                resolve(user);
+            } catch (err) {
+                reject(`User with email: ${email} not found`);
+            }
+        });
+    }
+
+    static async findByUsername(username) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const user = Schema.User.findOne({ username: username });
+                resolve(user);
+            } catch (err) {
+                reject(`User with username: ${username} not found`);
+            }
+        });
+    }
+
+    static async create(data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const user = await Schema.User.create(data);
+                resolve(user);
+            } catch (err) {
+                console.log(err)
+                reject('object could not be created');
+            }
+        });
+    };
+
+    destroy() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //let result = await // delete from database [this.id]
+                //resolve (objected was deleted);
+            } catch (err) {
+                reject('object could not be deleted')
+            }
+        })
+    };
+
+    static async update(objectData) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //const {id, title, etc} = objectData
+                //let result = await // insert into database [id, title, etc]
+                //resolve (result);
+            } catch (err) {
+                reject('object could not be updated');
+            }
+        });
+    };
+
+    static async addUser(objectData) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //const hashedPassword = await bcrypt.hash(objectData.body.password, 10)
+                //const user = {name: objectData.name, password: hashedPassword}
+                //let result = await // insert into database [user]
+
+
+
+
+                //const {id, title, etc} = objectData
+                //let result = await // insert into database [id, title, etc]
+                //resolve (result);
+            } catch (err) {
+                reject('user could not be added');
+            }
+        });
+    };
+
+    static async login(email, password) {
+        const user = await this.findByEmail(email);
+        if (user) {
+            const auth = await bcrypt.compare(password, user.password)
+            if (auth) {
+                return user;
+            }
+            throw Error('Invalid password')
+        }
+        throw Error('Incorrect email')
+    }
+
+    // static async getUser(objectData){
+    //     return new Promise (async (resolve, reject) => {
+    //         try {
+    //             const user = this.getById(user => user.name === objectData.body.name)
+    //             if (user == null) {
+    //                 return error('user could not be found')
+    //             }
+    //             try {
+    //                 if(bcrypt.compare(objectData.body.password, user.password)){
+    //                     console.log('woo');
+    //                 } else {
+    //                     console.log('not allowed')
+    //                 }
+    //             } catch {
+    //                 return error('user could not be found')
+    //             }
+    //         } catch (err) {
+    //             reject('user not found');
+    //         }
+    //     });
+    // };
 }
-
-const User = mongoose.model('user', userSchema);
-
-userSchema.plugin(uniqueValidator);
-
-module.exports = User;
