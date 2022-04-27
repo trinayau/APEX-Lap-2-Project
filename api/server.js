6835ae52df732d39ce1c825a72aea258c0409c5f
“scralfie” <kellyalfie6@gmail.com>
2022-04-27 17:17:18 +0100

    added error 404 page

--- api/server.js
+++ api/server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('*', checkUser);
 app.use('/games', gameRoutes);
 
@@ -25,6 +26,11 @@ app.get('/habitPage', requireAuth, (req, res) => res.render('habitPage', { title
 
 app.use(authRoutes);
 
+app.use((req, res) => {
+    res.status(404).render('404', {
+        title: "404"
+    })
+})
 //app.use('/search', searchRoutes);
 
 module.exports = app;
