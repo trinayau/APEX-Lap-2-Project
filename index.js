32fcc7af5262e6638ac44af2425296e8f118837a
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-30 12:42:11 +0000

    update mongodb uri

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Habit = require('./models/Habit');
 
 //const mongodbURI = process.env.DB_CONNECTION;
-const mongodbURI = 'mongodb+srv://apex-test:Test12@cluster0.8m7wy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongodbURI = 'mongodb+srv://apex-test:Test12@apexio.tgdvfhm.mongodb.net/?retryWrites=true&w=majority'
 const port = process.env.PORT || 3000;
 
 mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
